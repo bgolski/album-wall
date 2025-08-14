b79c48be201d79874bc6e09ed25a865d14b28a8e
Bradley Golski <bgolski@gmail.com>
2025-08-13 22:42:08 -0400

    15 clean up utils

--- src/utils/basePath.ts
+++ src/utils/basePath.ts
@@ -5,11 +5,3 @@
 export function getBasePath(): string {
   return process.env.NEXT_PUBLIC_BASE_PATH || "";
 }
-
-/**
- * Create absolute URL with proper base path
- */
-export function createUrl(path: string): string {
-  const basePath = getBasePath();
-  return `${basePath}${path}`;
-}
